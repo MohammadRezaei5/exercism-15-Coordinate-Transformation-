5a62e60ae06a85b48754b1a7d3270450ea7c8834
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-06 10:23:04 +0330

    Function: translate2d

--- coordinate-transformation.js
+++ coordinate-transformation.js
@@ -1,1 +1,11 @@
 /// look back again and again 🤐😐😑
+
+// const moveCoordinatesRight2Px = translate2d(2, 0);
+// const result = moveCoordinatesRight2Px(4, 8);
+
+function translate2d(dx, dy) {
+  return function translate(x, y) {
+    return [x + dx, y + dy];
+  };
+}
+// console.log(result);
